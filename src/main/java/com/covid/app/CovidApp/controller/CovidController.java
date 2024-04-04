d6b62de2dc5cbb3fdfdae4ac4c8760c138e7feb9
Archi jain <aarchijain2021@gmail.com>
2024-04-05 02:22:54 +0530

    Initial commit

---
+++ src/main/java/com/covid/app/CovidApp/controller/CovidController.java
@@ -0,0 +1,24 @@
+package com.covid.app.CovidApp.controller;
+
+import com.covid.app.CovidApp.service.Coviddataservice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/covid-data")
+
+public class CovidController {
+    @Autowired
+    Coviddataservice covidDataService;
+
+    //localhost:8080/covid-data/citywise/delhi
+    @GetMapping("/citywise/{city}")
+    public String getCovidCases(@PathVariable String city) {
+        Integer covidData = covidDataService.getCovidData(city);
+        return "Covid Active Cases in " + city + " = " + covidData;
+    }
+
+}
